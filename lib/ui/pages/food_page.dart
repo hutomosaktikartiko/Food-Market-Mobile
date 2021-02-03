20ffd8ebe2812d855c701cfc95a5eff40f4ad20c
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-02-03 10:30:30 +0700

    Flutter UI

---
+++ lib/ui/pages/food_page.dart
@@ -0,0 +1,124 @@
+part of 'pages.dart';
+
+class FoodPage extends StatefulWidget {
+  @override
+  _FoodPageState createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double listItemWidth =
+        MediaQuery.of(context).size.width - 2 * defaultMargin;
+
+    return ListView(
+      children: [
+        Column(
+          children: [
+            //// HEADER
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              color: Colors.white,
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Food Market',
+                        style: blackFontStyle1,
+                      ),
+                      Text(
+                        "Let's get some foods",
+                        style:
+                            greyFontStyle.copyWith(fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg'),
+                            fit: BoxFit.cover)),
+                  )
+                ],
+              ),
+            ),
+            //// LIST OF FOOD
+            Container(
+              height: 258,
+              width: double.infinity,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Row(
+                    children: mockFoods
+                        .map((e) => Padding(
+                              padding: EdgeInsets.only(
+                                  left: (e == mockFoods.first)
+                                      ? defaultMargin
+                                      : 0,
+                                  right: defaultMargin),
+                              child: FoodCard(e),
+                            ))
+                        .toList(),
+                  )
+                ],
+              ),
+            ),
+            //// LIST OF FOOD (TABS)
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  CustomTabBar(
+                    titles: ['New Taste', 'Popular', 'Recommended'],
+                    selectedIndex: selectedIndex,
+                    onTap: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Builder(builder: (_) {
+                    List<Food> foods = (selectedIndex == 0)
+                        ? mockFoods
+                        : (selectedIndex == 1) ? [] : [];
+
+                    return Column(
+                      children: foods
+                          .map((e) => Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    defaultMargin, 0, defaultMargin, 16),
+                                child: FoodListItem(
+                                    food: e, itemWidth: listItemWidth),
+                              ))
+                          .toList(),
+                    );
+                  }),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 80,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,60 @@
+part of 'pages.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int selectedPage = 0;
+  PageController pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+          SafeArea(
+              child: Container(
+            color: 'FAFAFC'.toColor(),
+          )),
+          SafeArea(
+              child: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                selectedPage = index;
+              });
+            },
+            children: [
+              Center(
+                child: FoodPage(),
+              ),
+              Center(
+                child: OrderHistoryPage(),
+              ),
+              Center(
+                child: ProfilePage(),
+              ),
+            ],
+          )),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: CustomBottomNavBar(
+              selectedIndex: selectedPage,
+              onTap: (index) {
+                setState(() {
+                  selectedPage = index;
+                });
+                pageController.jumpToPage(selectedPage);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
