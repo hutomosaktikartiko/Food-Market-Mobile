d144d80dcf72e74788826e391cb156afccaf5c95
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-02-26 11:26:11 +0700

    User Cubit - Sign Up

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:food_market/models/models.dart';
@@ -10,6 +12,17 @@ class UserCubit extends Cubit<UserState> {
 
   Future<void> signIn(String email, String password) async {
     ApiReturnValue<User> result = await UserServices.signIn(email, password);
+
+    if (result.value != null) {
+      emit(UserLoaded(result.value));
+    } else {
+      emit(UserLoadingFailed(result.message));
+    }
+  }
+
+  Future<void> signUp(User user, String password, {File pictureFile}) async {
+    ApiReturnValue<User> result =
+        await UserServices.signUp(user, password, pictureFile: pictureFile);
 
     if (result.value != null) {
       emit(UserLoaded(result.value));
