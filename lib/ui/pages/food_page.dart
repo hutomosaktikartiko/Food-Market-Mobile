748f57051c9dc4bb71eb38ae0357e8e179bcf374
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-03-01 22:20:30 +0700

    Update Food Page

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -69,7 +69,7 @@ class _FoodPageState extends State<FoodPage> {
                             children: state.foods
                                 .map((e) => Padding(
                                       padding: EdgeInsets.only(
-                                          left: (e == mockFoods.first)
+                                          left: (e == state.foods.first)
                                               ? defaultMargin
                                               : 0,
                                           right: defaultMargin),
