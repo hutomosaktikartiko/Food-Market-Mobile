b2c2d31f6ef3fc625fc349c37fa9c74349a8add3
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-03-01 22:08:18 +0700

    Food Services - get Food

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -22,6 +22,27 @@ class Food extends Equatable {
       this.types = const [],
       this.rate});
 
+  factory Food.fromJson(Map<String, dynamic> data) => Food(
+      id: data['id'],
+      picturePath: data['picturePath'],
+      name: data['name'],
+      description: data['description'],
+      ingredients: data['ingredients'],
+      price: data['price'],
+      rate: (data['rate'] as num).toDouble(),
+      types: data['types'].toString().split(',').map((e) {
+        switch (e) {
+          case 'recommended':
+            return FoodType.recomended;
+            break;
+          case 'popular':
+            return FoodType.popular;
+            break;
+          default:
+            return FoodType.new_food;
+        }
+      }).toList());
+
   @override
   List<Object> get props =>
       [id, picturePath, name, description, ingredients, price, types, rate];
